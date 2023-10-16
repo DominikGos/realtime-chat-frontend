427e95f666143933ed59ceb846b6587a1d689eb7
Dominik <dominikgos3@gmail.com>
2023-10-16 23:33:35 +0200

    installed vuex

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import './assets/main.sass'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex';
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(Vuex)
 
 app.mount('#app')
