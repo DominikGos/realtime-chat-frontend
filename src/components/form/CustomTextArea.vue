d4f7708a08509118a62cfcb851c1328b3e548d26
Dominik <dominikgos3@gmail.com>
2023-10-19 14:18:15 +0200

    added invalid state in the CustomTextArea component

--- src/components/form/CustomTextArea.vue
+++ src/components/form/CustomTextArea.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps({
 	modelValue: String,
+	invalid: Boolean
 })
 
-defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue']);
+
+function handleInputEvent(event: Event) {
+	emit('update:modelValue', (event.target as HTMLInputElement).value);
+}
+
+const classes = computed<string>(() => {
+	let classes = 'w-full bg-gray-100 rounded-md p-2 shadow-md focus:outline-none border border-transparent transition duration-300 ease-in  focus:border-gray-300 focus:bg-white focus:shadow-none '
+
+	if (props.invalid)
+		classes += 'border-red-500 shadow-none bg-white focus:border-red-500'
+
+	return classes;
+});
 </script>
 
 <template>
-	<textarea :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-		class="w-full bg-gray-100 rounded-md p-2 shadow-md focus:outline-none border border-transparent transition duration-300 ease-in  focus:border-gray-300 focus:bg-white focus:shadow-none"></textarea>
+	<textarea :value="modelValue" @input="handleInputEvent" :class="classes"></textarea>
 </template>
