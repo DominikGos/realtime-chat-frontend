d992c4a6b909c71af7156240a70e345709b1bcb2
Dominik <dominikgos3@gmail.com>
2023-10-19 13:48:14 +0200

    added invalid state in the CustomIput component

--- src/components/form/CustomInput.vue
+++ src/components/form/CustomInput.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps({
   modelValue: String,
+  invalid: Boolean
 });
 
-defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+const classes = computed<string>(() => {
+  let classes = 'w-full bg-gray-100 rounded-md p-2 shadow-md focus:outline-none border border-transparent transition duration-300 ease-in  focus:border-gray-300 focus:bg-white focus:shadow-none '
+  
+  if(props.invalid) 
+    classes += 'border-red-500 shadow-none bg-white focus:border-red-500'
+
+  return classes;
+});
+
+function handleInputEvent(event: Event) {
+  emit('update:modelValue', (event.target as HTMLInputElement).value);
+}
 </script>
 
 <template>
-  <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-    class="w-full bg-gray-100 rounded-md p-2 shadow-md focus:outline-none border border-transparent transition duration-300 ease-in  focus:border-gray-300 focus:bg-white focus:shadow-none" autocomplete="on" />
+  <input :value="modelValue" @input="handleInputEvent" :class="classes" autocomplete="on"/>
 </template>
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -8,7 +8,7 @@ import type User from '@/interfaces/User';
 import AuthService from '@/services/AuthService';
 
 const auth = new AuthService;
-const errors = ref({});
+const errors = ref();
 const formProcessing = ref();
 const userCredentials = ref({
   email: '',
@@ -34,12 +34,12 @@ async function submit() {
       </header>
       <div class="flex flex-col gap-3">
         <InputLabel>E-mail</InputLabel>
-        <CustomInput type="email" v-model="userCredentials.email" required/>
+        <CustomInput type="email" v-model="userCredentials.email" :invalid="errors?.email" required/>
         <InputError v-if="errors?.email">{{ errors.email }}</InputError>
       </div>
       <div class="flex flex-col gap-3">
         <InputLabel>Password</InputLabel>
-        <CustomInput type="password" v-model="userCredentials.password" required/>
+        <CustomInput type="password" v-model="userCredentials.password" :invalid="errors?.password" required/>
         <InputError v-if="errors?.password">{{ errors.password }}</InputError>
       </div>
       <div>
