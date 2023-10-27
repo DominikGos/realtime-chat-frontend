4fb1499dbd53366555603a04ddeb12162f82d2f6
Dominik <dominikgos3@gmail.com>
2023-10-27 22:34:29 +0200

    added lastMessage function

--- src/components/panels/ChatsPanel.vue
+++ src/components/panels/ChatsPanel.vue
@@ -6,6 +6,7 @@ import ChatService from '@/services/ChatService';
 import type Chat from '@/interfaces/Chat';
 import type User from '@/interfaces/User';
 import { store } from '@/store';
+import type Message from '@/interfaces/Message';
 
 const chatService: ChatService = new ChatService;
 const chats = ref<Chat[]>([]);
@@ -37,6 +38,20 @@ const chatList = computed<Chat[]>(() => {
   return chatList;
 })
 
+function lastMessage(message: Message): string {
+  let lastMessage: string = '';
+  let authorName: string = message.user.id == store.state.auth.user.id 
+    ? 'you'
+    : `${message.user.first_name} ${message.user.last_name}`;
+
+  if(message.text) 
+    lastMessage = `${authorName}: ${message.text}`
+  else if(message.files_links.length > 0) 
+    lastMessage = `${authorName}: sent files(${message.files_links.length})`
+  
+  return lastMessage;
+}
+
 async function setChat(chat: Chat) {
   store.commit('setChat', chat); 
 }
@@ -46,18 +61,18 @@ await loadChats(offset);
 
 <template>
   <div class="flex flex-col gap-3 pb-5 overflow-y-scroll h-[calc(100%-94px)] scrollbar-thin scrollbar-thumb-gray-300 overflow-hidden">
-    <PanelItem v-for="chat in chatList" :key="chat.id" @click="setChat(chat)" :active="chat.id == store.state.components.chat.id">
+    <PanelItem v-for="chat in chatList" :key="chat.id" @click="setChat(chat)" :active="chat?.id == store.state.components.chat?.id">
       <template v-slot:start>
         <Avatar :size="'medium'" :avatar="getFriend(chat.users)?.avatar_link"/>
       </template>
       <template v-slot:middle>
         <p>{{ getFriend(chat.users)?.first_name }} {{ getFriend(chat.users)?.last_name }}</p>
-        <p v-if="chat.last_message?.text" class="text-gray-400 truncate w-[calc(100%-60px)]"> {{ chat.last_message.text }} </p>
-        <p v-else-if="chat.last_message?.files_links" class="text-gray-400 truncate w-[calc(100%-60px)]"> sent files {{ `(${chat.last_message.files_links.length})`}} </p>
+        <p class="text-gray-400 truncate w-[calc(100%-60px)]"> {{ lastMessage(chat.last_message! ) }} </p>
       </template>
       <template v-slot:end>
         <p class="text-xs">{{ chat.last_message?.created_at }}</p>
       </template>
     </PanelItem>
+    <p v-if="(! loading) && chatList.length === 0" class="text-sm text-center">You have no any conversations.</p>
   </div>
 </template>
