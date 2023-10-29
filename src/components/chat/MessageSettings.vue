ea1f9a1bade02a8338c2f6c6683fc1ec9901d4fc
Dominik <dominikgos3@gmail.com>
2023-10-29 17:54:29 +0100

    added message settings component

---
+++ src/components/chat/MessageSettings.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center p-2">
+    <div class="flex flex-col p-3 shadow-md rounded-md border gap-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
--- src/components/chat/MessageWrapper.vue
+++ src/components/chat/MessageWrapper.vue
@@ -5,6 +5,9 @@ import FileWrapper from './FileWrapper.vue';
 import type MessageInterface from '@/interfaces/Message';
 import type User from '@/interfaces/User';
 import { store } from '@/store';
+import { ref } from 'vue';
+import MessageSettings from './MessageSettings.vue';
+import CustomButton from '../buttons/CustomButton.vue';
 
 const props = defineProps<{
   message: MessageInterface,
@@ -12,23 +15,30 @@ const props = defineProps<{
 
 const authUser: User = store.state.auth.user;
 const userIsAuthor: Boolean = authUser.id === props.message.user.id;
+const showSettings = ref(false);
 </script>
 
 <template>
   <div :class="[userIsAuthor ? 'justify-end' : 'justify-start', 'flex']">
+    <Transition name="fade">
+      <MessageSettings v-if="showSettings">
+        <CustomButton :color="'red'">Delete</CustomButton>
+        <CustomButton :color="'gray'" @click="showSettings = false">Close</CustomButton>
+      </MessageSettings>
+    </Transition>
     <div class="p-3 flex flex-col h-min  max-w-[256px] ">
       <div class="flex gap-3 items-end relative">
         <div v-if="userIsAuthor === false">
-          <Avatar :size="'small'" :avatar="message.user.avatar_link"/>
+          <Avatar :size="'small'" :avatar="message.user.avatar_link" />
         </div>
         <div v-if="userIsAuthor" class="h-full absolute flex items-center p-3 -left-6">
-          <button>
+          <button @click="showSettings = ! showSettings">
             <i class="fa-solid fa-ellipsis-vertical"></i>
           </button>
         </div>
         <div class="flex flex-col gap-2">
-          <Message v-if="message.text" :message="message" :userIsAuthor="userIsAuthor"/>
-          <FileWrapper v-if="message.files_links.length > 0" :filesLinks="message.files_links"/>
+          <Message v-if="message.text" :message="message" :userIsAuthor="userIsAuthor" />
+          <FileWrapper v-if="message.files_links.length > 0" :filesLinks="message.files_links" />
         </div>
       </div>
       <div :class="[userIsAuthor ? 'justify-start' : 'justify-end ', 'h-8 flex p-2']">
