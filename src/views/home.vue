e7d33d56b586e052972cbaf7bd989c0d4dde74a3
Dominik <dominikgos3@gmail.com>
2023-10-31 17:14:16 +0100

    added listenng for a channel chat.2

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,6 +4,33 @@ import MainNavbar from '../components/MainNavbar.vue';
 import Chat from '../components/chat/Chat.vue';
 import { store } from '@/store';
 import ProfileVue from '@/components/Profile.vue';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+import AuthService from '../services/AuthService';
+
+
+const authService = new AuthService;
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    authEndpoint: 'http://realtime-chat/broadcasting/auth',
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    auth: {
+        headers: {
+            Authorization: 'Bearer ' + authService.getUserFromTheBrowser()?.token
+        },
+    },
+
+});
+
+window.Echo.private('chat.2')
+    .listen('MessageSent', (e: any) => {
+        console.log(e);
+    })  
+
 </script>
 
 <template>
