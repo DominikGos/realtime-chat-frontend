1cf37dd6fa226c56cc2ed96050dd4f0c8e398e90
Dominik <dominikgos3@gmail.com>
2023-11-14 20:46:01 +0100

    changed panel height on the small devices

--- src/components/panels/Panel.vue
+++ src/components/panels/Panel.vue
@@ -25,7 +25,7 @@ function showSearchedUsers(): void {
 
 <template>
   <div
-    class="w-full h-[calc(100%-50px)] p-4 bg-white lg:h-full lg:shadow-lg lg:w-72 lg:min-w-[18rem] xl:w-80 xl:min-w-[20rem] 2xl:w-96 2xl:min-w-[24rem]">
+    class="w-full h-[calc(100%-64px)] p-4 bg-white lg:h-full lg:shadow-lg lg:w-72 lg:min-w-[18rem] xl:w-80 xl:min-w-[20rem] 2xl:w-96 2xl:min-w-[24rem]">
     <header class="mb-5 w-full">
       <Transition name="fade" mode="out-in">
         <h3 class="font-semibold text-2xl" :key="store.state.components.panel.name">
