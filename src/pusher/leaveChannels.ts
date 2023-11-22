7e656ca1a284c9d64502c67179c1e95ba1317872
Dominik <dominikgos3@gmail.com>
2023-11-22 17:15:06 +0100

    added conditions in the leaveChannels function, user leave channel if in the state are his data and echo library is defined

--- src/pusher/leaveChannels.ts
+++ src/pusher/leaveChannels.ts
@@ -1,12 +1,20 @@
 import type User from "@/interfaces/User";
 import { store } from "@/store";
 
-export function leaveChannels(authUser: User): void {
-    store.state.auth.chatsIds.forEach((chatId: number) => {
-        window.Echo.leave(`chat.${chatId}`);
-    })
+export function leaveChannels(authUser?: User): void {
+    if( ! window.Echo) {
+        return;
+    }
+    
+    if(store.state.auth.chatsIds) {
+        store.state.auth.chatsIds.forEach((chatId: number) => {
+            window.Echo.leave(`chat.${chatId}`);
+        })
+    }
 
-    window.Echo.leave(`new.chat.with.user.${authUser.id}`);
+    if(authUser) {
+        window.Echo.leave(`new.chat.with.user.${authUser.id}`);
+    }
 
     window.Echo.leave('user.updated');
 }
--- src/pusher/listeners/user.ts
+++ src/pusher/listeners/user.ts
@@ -3,8 +3,6 @@ import { store } from "@/store";
 export function listenUsers() {
     window.Echo.channel(`user.updated`)
         .listen('UserUpdated', (e: any) => {
-            console.log('??');
-            
             store.commit('setUpdatedUser', e.user)
         })
 }
