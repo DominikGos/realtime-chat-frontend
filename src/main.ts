d090b1b3d596c19ba152f958018bd7f1c2558f84
DominikGos <dominikgos3@gmail.com>
2024-03-15 22:53:43 +0100

    The user leaves previously connected channels when the page is loaded

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.sass';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios';
 import { store } from './store';
 import AxiosService from './services/AxiosService';
 
